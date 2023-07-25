dbc1bfb189284a15ffbbbb2388e265fb590ea076
Kai <kai.zahn@yahoo.de>
2023-07-25 10:01:18 +0200

    1.18.2 change

--- src/main/java/de/cadentem/cave_dweller/CaveDweller.java
+++ src/main/java/de/cadentem/cave_dweller/CaveDweller.java
@@ -12,6 +12,7 @@ import de.cadentem.cave_dweller.registry.ModSounds;
 import de.cadentem.cave_dweller.util.Utils;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
@@ -84,7 +85,7 @@ public class CaveDweller {
     }
 
     @SubscribeEvent
-    public void serverTick(final TickEvent.ServerTickEvent event) {
+    public void serverTick(final TickEvent.WorldTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
             // Prevent ticking twice per server tick
             return;
@@ -98,7 +99,14 @@ public class CaveDweller {
             LOG.info("Values have been reloaded");
         }
 
-        ServerLevel overworld = event.getServer().getLevel(Level.OVERWORLD);
+        MinecraftServer server = event.world.getServer();
+
+        if (server == null) {
+            LOG.debug("Server was null within the `serverTick`");
+            return;
+        }
+
+        ServerLevel overworld = server.getLevel(Level.OVERWORLD);
 
         if (overworld == null) {
             return;
