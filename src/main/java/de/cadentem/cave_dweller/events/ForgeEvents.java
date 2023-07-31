7b5a1c13b5ccda06367fe76d0e9b9d180ec91986
Kai <kai.zahn@yahoo.de>
2023-07-31 17:58:00 +0200

    dimensions

--- src/main/java/de/cadentem/cave_dweller/events/ForgeEvents.java
+++ src/main/java/de/cadentem/cave_dweller/events/ForgeEvents.java
@@ -4,6 +4,7 @@ import de.cadentem.cave_dweller.CaveDweller;
 import de.cadentem.cave_dweller.entities.CaveDwellerEntity;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -39,5 +40,11 @@ public class ForgeEvents {
                 event.setCanceled(true);
             }
         }
+    }
+
+    /** Not all dimensions are immediately loaded */
+    @SubscribeEvent
+    public static void handleDimensionChange(final EntityTravelToDimensionEvent event) {
+        CaveDweller.RELOAD_MISSING = true;
     }
 }
